37eae80950449119b6856a46f3141f79968b8d04
SubsectorKirisakyia <108776309+SubsectorKirisakyia@users.noreply.github.com>
2025-07-24 14:39:05 +0700

    first commit

---
+++ src/test/java/com/arcyriea_loreverse/oracle_cloud/OracleCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcyriea_loreverse.oracle_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
